aa9b6e8d219a6963bcfb2044fd01c4aacb813f39
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-10-07 11:01:49 +0600

    Runnables Passthrough & Parallel

---
+++ 6.Lanchain Runnables/3.Runnable_PassThrough.py
@@ -0,0 +1,33 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+
+load_dotenv()
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+model = ChatGroq(groq_api_key=GROQ_API_KEY, model_name='llama-3.3-70b-versatile')
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+print(final_chain.invoke({'topic':'cricket'}))
