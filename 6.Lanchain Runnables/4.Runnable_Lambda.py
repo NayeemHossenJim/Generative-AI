91cf0b363e82e9324f54e71fec4b22bda6b6788e
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-10-07 11:11:17 +0600

    Lambda

---
+++ 6.Lanchain Runnables/4.Runnable_Lambda.py
@@ -0,0 +1,35 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
+
+load_dotenv()
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+model = ChatGroq(groq_api_key=GROQ_API_KEY, model_name='llama-3.3-70b-versatile')
+
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'word_count': RunnableLambda(word_count)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
